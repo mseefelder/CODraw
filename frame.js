55867f1877192cb2090287fa321af3ec35f87305
mseefelder <saa.marcos@gmail.com>
2014-05-27 17:04:13 -0300

    Some addition to the code, nothing major

--- frame.js
+++ frame.js
@@ -3,12 +3,21 @@
 	For now it draws many circles while the LMB is pressed.
 	BUG: when the mouse goes off the canvas, isDraw should 
 turn false.
+
+	Planned workflow:
+	For every update:
+		clear canvas
+		draw local image
+		draw external shapes
+		update local image
+		draw internal shapes
 */
 
 var canvas = document.getElementById('canv');
 var frame = canvas.getContext('2d');
 var isDraw = false;
 var bSize = 1;
+var localImage = 0;
 
 function drawSquare(mx,my){
 	frame.fillRect(mx,my,80,80);
@@ -41,4 +50,38 @@ function getMouse(canvas, evt) {
     };
 }
 
+//tentativas
+
+function getMouseF(canvas) {
+	var rect = canvas.getBoundingClientRect();
+    return {
+    	x: frame.clientX - rect.left,
+        y: frame.clientY - rect.top
+    };
+}
+
+function drawElements(){
+
+}
+
+function update(){
+	canvas.width = canvas.width; //to clear the canvas
+	if (localImage) frame.putImageData(0,0,canvas.width, canvas.height);
+	drawElements();
+	localImage = frame.getImageData(0,0,canvas.width, canvas.height);
+}
+
+function mainloop(){
+	var mouse = getMouseF(canvas);
+	if (isDraw) drawBrush(mouse.x,mouse.y);
+}
+
 //setInterval(mainloop, 1000/60);
+
+
+/*EXTRAS:
+Super good tutorial:
+diveintohtml5.info/canvas.html
+Image Data:
+http://www.w3schools.com/tags/canvas_putimagedata.asp
+*/
