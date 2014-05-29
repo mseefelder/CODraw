f169d54ce07789fc4026a5b306eed9c2225e9df4
PedroHB <pedrohb@pedrohb-Inspiron-5423.(none)>
2014-05-29 14:47:32 -0300

    client.c v2 (added ableto_write, mesg_sent, etc

--- CODraw-C/client.c
+++ CODraw-C/client.c
@@ -6,14 +6,17 @@
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
+#define MESSAGESIZE 2000;
  
 int main(int argc , char *argv[])
 {
+    bool ableto_write = 0;
+    bool message_sent = 0;  //new!
     int sock;
     int msgtype;
     int size;
     struct sockaddr_in server;
-    char message[2000] , server_reply[2000];
+    char message[MESSAGESIZE] , server_reply[MESSAGESIZE];
     
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -39,7 +42,7 @@ int main(int argc , char *argv[])
     msgtype = 0;
     while(msgtype != 2)
     {
-        size = recv(sock , server_reply , 2000 , 0);
+        size = recv(sock , server_reply , MESSAGESIZE , 0);
         printf("OK %d\n", size);
         if(size > 0)
         {
@@ -56,29 +59,57 @@ int main(int argc , char *argv[])
     while(1)
     {
 
-
-        printf("Enter message: ");
-        scanf("%s" , message);
-         
-        //Send some data
-        if(send(sock , message , strlen(message) , 0) < 0)
+        if (ableto_write)
         {
-            puts("Send failed");
-            return 1;
-        }
+            printf("Enter message: ");
+            scanf("%s" , message);
          
-        int message_size = (int) sizeof(message);
-        printf("%d\n", message_size);
+
+        //Send some data
+            if(send(sock , message , strlen(message) , 0) < 0)
+            {
+                puts("Send failed");
+                return 1;
+            }
+        
+            int message_size = (int) sizeof(message);
+            printf("%d\n", message_size);
+        }    
 
         //Receive a reply from the server
-        size = recv(sock , server_reply , 2000 , 0);
-        printf("OK %d\n", size);
+        size = recv(sock , server_reply , MESSAGESIZE , 0);    //server reply could be anything, as long it verifies the server-client conection
+        //printf("OK %d\n", size);  //not needed
         if(size < 0)
         {
             puts("recv failed");
             break;
         }
-        
+        else
+        { 
+            if(server_reply[0] == '1')  //send acknowledge, server received client's message  (server_reply = 1) ~ou algo parecido
+            {
+                message_sent = 1;
+            }
+            else if (server_reply[0] == '2' && message_sent != 1) //broadcast
+            {
+                printf("%s\n", server_reply);  // (server_reply = 2 - userN: message)   [this chat can only send once everybody received las sent]
+                strcpy(message, "ack");                                             //  [message. Therefore server_reply contains only one message]
+                if(send(sock , message , strlen(message) , 0) < 0) //sends acknowledge to server
+                {
+                    puts("Could not reply to server");
+                    return 1
+                }
+            else if (server_reply[0] == '3')  // (server_reply = 3) server received all acknowledges
+            {
+                ableto_write = 1;
+            }
+            else if (server_reply[0] == '4')  // (server_reply = 4) server is yet to received all acknowledges
+            {
+                ableto_write = 0;          
+            }
+
+        }
+        message_sent = 0;
         server_reply[size] = 0;
         printf("\nServer Reply: %s\n", server_reply);
         memset(&message[0], 0, sizeof(message));
