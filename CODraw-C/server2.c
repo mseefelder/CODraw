139bac0addb29839f570de0ee5f62c7fbea2c1ce
phpalmeiro <phpalmeiro@gmail.com>
2014-05-29 14:41:40 -0300

    Adding Changes in 'server2.c'

--- CODraw-C/server2.c
+++ CODraw-C/server2.c
@@ -5,66 +5,71 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<pthread.h>
+#include<linkedlist.h>
 
 #define USERNAME_SIZE 20;
 #define MESSAGE_SIZE 2000;
- 
+
+//Declaring the Thread Handler and Broadcast Functions 
 void *connection_handler(void *);
 void broadcast(int);
+Node *socket_list;
  
 int main(int argc , char *argv[])
 { 
-    int socket_desc , client_sock , c , *new_sock;
-    struct sockaddr_in server , client;
-     
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    int socket_desc, client_sock, c, *new_sock;
+    struct sockaddr_in server, client;
+
+    //Creates Socket     
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1)
     {
-        printf("Could not create socket");
+        printf("Could Not Create Socket");
     }
-    printf("Socket created");
-     
+    printf("Socket Created");
+    
+    socket_list = newList();
+
+    //Does Some Magic 
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons( 8888 );
-     
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    
+    //Stabilishes the Bind 
+    if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
-        //print the error message
         perror("Bind Failed. Error");
         return 1;
     }
     printf("Bind Done");
      
-    //Listen
     listen(socket_desc , 3);
      
-    //Accept and incoming connection
+    //Waiting for Connection Request and Acceptance of Such Requests
     printf("Waiting for Incoming Connections...");
     c = sizeof(struct sockaddr_in);
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
-        printf("Conexão Aceita");
+        printf("Connection Accepted");
          
         pthread_t sniffer_thread;
         new_sock = malloc(1);
         *new_sock = client_sock;
          
-        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        if(pthread_create(&sniffer_thread, NULL,  connection_handler, (void)new_sock) < 0)
         {
-            perror("Could not create thread");
+            perror("Could Not Create Thread");
             return 1;
         }
          
         //Now join the thread , so that we dont terminate before the thread
         //pthread_join( sniffer_thread , NULL);
-        printf("Handler assigned");
+        printf("Handler Assigned");
     }
      
     if (client_sock < 0)
     {
-        perror("accept failed");
+        perror("Accept Failed");
         return 1;
     }
      
@@ -73,12 +78,24 @@ int main(int argc , char *argv[])
 
 void *connection_handler(void *socket_desc)
 {
+    //Declaring Socket Descriptor and Some Other Stuff
     int sock = *(int*)socket_desc;
     char username[USERNAME_SIZE], server_message[MESSAGE_SIZE], client_message[MESSAGE_SIZE];
+    User new_user;
 
-    strcpy(server_message, "Welcome to the CatSkin Messenger! Enter your Username.\n");
+    //First Message of Connection
+    strcpy(server_message, "Welcome to the Koi Fish Messenger! Enter your Username:\n");
     send(sock, server_message, strlen(server_message), 0);
 
+    //Receives 
+    if(read_size = recv(sock, username, USERNAME_SIZE, 0) > 0)
+    {
+        new_user.sock = sock;
+        new_user.name = username;
+        insert(&socket_list, &new_user);
+    }
+
+    //Client Message Receiving
     while(1);
     {
         if(read_size = recv(sock, client_message, MESSAGE_SIZE, 0) > 0)
@@ -86,13 +103,21 @@ void *connection_handler(void *socket_desc)
 
         }
 
+        //Erases the Message Vectors
         memset(&server_message[0], 0, MESSAGE_SIZE);
         memset(&client_message[0], 0, MESSAGE_SIZE);
 
     }
 }
 
-void
+void broadcast(void *socket_list)
+{  
+    //Declaring Socket Descriptor
+    int sock = *(int*)socket_desc;
+    
+    for(i = 0; i < sizeof(socket_list); i++)
+    send()
+}
 
 
 
