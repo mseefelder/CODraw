a5e0e3a5d825b6090b6e7e42176126591ff15624
PedroHB <pedrohb@pedrohb-Inspiron-5423.(none)>
2014-05-29 16:08:58 -0300

    client.c fixes and greetings implementation (for real)

--- CODraw-C/client.c
+++ CODraw-C/client.c
@@ -7,14 +7,14 @@
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
 
-#define MESSAGESIZE 2000;
+#define MESSAGESIZE 2000
 
-typedef enum{false,true} bool; 
+typedef enum{False,True} mybool; 
  
 int main(int argc , char *argv[])
 {
-    bool ableto_write = false;
-    bool message_sent = false;  //new!
+    mybool ableto_write = False;
+    mybool message_sent = False;  //new!
     int sock;
     int msgtype;
     int size;
@@ -98,28 +98,29 @@ int main(int argc , char *argv[])
         { 
             if(server_reply[0] == '1')  //send acknowledge, server received client's message  (server_reply = 1) ~ou algo parecido
             {
-                message_sent = true;
+                message_sent = True;
             }
-            else if (server_reply[0] == '2' && message_sent != true) //broadcast
+            else if (server_reply[0] == '2' && message_sent != True) //broadcast
             {
                 printf("%s\n", server_reply);  // (server_reply = 2 - userN: message)   [this chat can only send once everybody received las sent]
                 strcpy(message, "ack");                                             //  [message. Therefore server_reply contains only one message]
                 if(send(sock , message , strlen(message) , 0) < 0) //sends acknowledge to server
                 {
                     puts("Could not reply to server");
-                    return 1
+                    return 1;
                 }
+            }
             else if (server_reply[0] == '3')  // (server_reply = 3) server received all acknowledges
             {
-                ableto_write = true;
+                ableto_write = True;
             }
             else if (server_reply[0] == '4')  // (server_reply = 4) server is yet to received all acknowledges
             {
-                ableto_write = false;          
+                ableto_write = False;          
             }
 
         }
-        message_sent = 0;
+        message_sent = False;
         server_reply[size] = 0;
         printf("\nServer Reply: %s\n", server_reply);
         memset(&message[0], 0, sizeof(message));
@@ -128,6 +129,6 @@ int main(int argc , char *argv[])
      
     close(sock);
     return 0;
+    
 }
-
-
+    
